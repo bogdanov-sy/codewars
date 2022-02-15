4aa5239bd94d41e99b3efcb97b0ed6255b1e163b
bogdanov.s <bogdanov.sy@gmail.com>
2022-02-15 07:42:48 +0300

    Array.diff - final

--- main.py
+++ main.py
@@ -3,17 +3,12 @@ b = [2, 4]
 
 def array_diff(a, b):
     #your code here
-    z = 0
-    c = a
-    print(c)
-    for x in c:
+    c = []
+    for x in a:
         print(x)
-        for y in b:
-            print('--' + str(y))
-            if x == y:
-                del a[z]
-                print(a)
-        z = z + 1
-    return(a)
+        if x not in b:
+            c.append(x)
+            print(c)
+    return(c)
 
 print(array_diff(a, b))
