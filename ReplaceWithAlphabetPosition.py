dc79dc53479365eda5d471bfab19a61277f04044
bogdanov.s <bogdanov.sy@gmail.com>
2022-03-06 12:49:44 +0300

    alphabet_position - work

--- ReplaceWithAlphabetPosition.py
+++ ReplaceWithAlphabetPosition.py
@@ -3,13 +3,19 @@ def alphabet_position(text):
     ABC = tuple('ABCDEFGHIJKLMNOPQRSTUVWXYZ')
     print(ABC)
     code = ''
+    counter = 0
     for i in text:
         print(i)
         if i.upper() in ABC:
             print(i.upper())
             index = ABC.index(i.upper()) + 1
             print(index)
-            code += ' ' + str(index)
+            if counter == 0:
+                code += str(index)
+            else:
+                code += ' ' + str(index)
+        counter += 1
+        print(counter)
     #code_str = ''.join(code)
     print(code)
     return(code)
