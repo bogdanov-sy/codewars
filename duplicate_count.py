d1f7f2646d63900e437b85f07a444e710f4da863
Bogdanov Sergey <bogs@yandex.ru>
2022-03-12 00:04:01 +0300

    duplicate_count - нужно схлопнуть одинаковые символы в str

--- duplicate_count.py
+++ duplicate_count.py
@@ -1,10 +1,21 @@
 def duplicate_count(text):
     # Your code goes here
+
     count = 0
-    for i in text:
-        for b in text:
-            if i.lower() == b.lower():
-                count += 1
+    len_text = len(str(text))
+    print(len_text)
+    list_text = list(text)
+    print(list_text)
+    for i in range(1, len_text):
+        b = list_text.pop()
+        print(list_text)
+        print(b)
+        count += list_text.count(b)
+        print(count)
+        #for x in list_text:
+        #    print(x)
+        #    if x == b:
+        #        count += 1
     print(count)
     return (count)
 
